53de84222f35392cfb7a4009bf4d30aea76a5939
f-s-h <fabian.heinrich@tum.de>
2026-07-29 09:01:32 +0200

    Implemented router

---
+++ web-client/src/router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
+
+export default router;
